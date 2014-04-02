50d0b2be193774187ddae1be83bce8beb53e08ee
Brian Kelley <Mac@Brians-MacBook-Pro.local>
2014-04-02 15:04:26 -0500

    completed landing page with the majority of some copy

--- index.php
+++ index.php
@@ -4,15 +4,9 @@
 
 <body>
 	
-	<header>
-		
-		<!-- Main Navigation -->
-		<?php include('views/partials/mainNavigation.php'); ?>
-		<!-- End Main Navigation -->
+	
 
-	</header>
-
-	<main class="sticky-compensation" role="main">
+	<main role="main">
 		
 		<section class="mogo-experience">
 			<div class="mogo-experience--container">
@@ -24,62 +18,218 @@
 			<div class="wrapper align-center">
 				<img src="images/logo-mogobox.png">
 				<p class="mvxs uppercase secondary-color great-text">Is Going Through A</p>
-				<img src="images/logo-mogo-makeover.png">
-			</div>
-		</section>
-
-		<section class="mogo-update">
-			<div class="wrapper align-center pvl">
+				<img class="mbxl" src="images/logo-mogo-makeover.png">
+	
 				<h3 class="uppercase great-text">Here is What's Taking Place</h3>
-				<p class="primer-text secondary-color">The ownership of Mogo has hired a new executive marketing director.  In hiring this new director, an updated vision and new passion is being brought to Mogo. To create the new vision that Mogo has we have temporarily pulled the account down to rebrand and structure the site to give a premier look, feel and experience to the Mogo Nation.</p>
-				<p class="primer-text semibold mbl">On July 1st, MogoBox  will have a launch - offering some of the best products you will ever see or find, along with a new Steal or Deal every month.</p>
+				<p class="paragon-text thin secondary-color">The ownership of Mogo has hired a new executive marketing director.  In hiring this new director, an updated vision and new passion is being brought to Mogo.</p>
+
+				<p class="paragon-text semibold mbl">To create the new vision that Mogo has we have temporarily pulled the account down to rebrand and structure the site to give a premier look, feel, and experience to the Mogo Nation.</p>
+
+				<p class="paragon-text semibold mbl">In July 2014, MogoBox will have a launch – offering some of the best products you will ever see or find, along with a new Steal or Deal every month.</p>
 			</div>
 		</section>
 
-		<!--TABS WILL GO HERE-->
+		<section class="mogo-tabs tabs">
+								
+				<section class="tabbed-nav--container">
+					<div class="wrapper">
+						<div class="tabbed-nav--headline align-center">
+							<h3 class="mtn canon-text capitalize white">What is the Mogo Experience?</h3>
+						</div>
+						<div class="tabbed-nav--interior">
+							<div class="tabbed-nav--row">
+							
+								<a data-reveal="tab1" class="is-current tabbed-nav--item mogosocial" href="#">
+									<div class="tabbed-nav--label">Mogo<span class="semibold">Social</span></div>
+								</a>
+								<a data-reveal="tab2" class="tabbed-nav--item mogoblog" href="#">
+									<div class="tabbed-nav--label">Mogo<span class="semibold">Blog</span></div>
+								</a>
+								<a data-reveal="tab3" class="tabbed-nav--item mogonation" href="#">
+									<div class="tabbed-nav--label">Mogo<span class="semibold">Nation</span></div>
+								</a>
+								<a data-reveal="tab4" class="tabbed-nav--item mogobox" href="#">
+									<div class="tabbed-nav--label">Mogo<span class="semibold">Box</span></div>
+								</a>
+								
+							</div>
+						</div>
+					</div>
+				</section>
+				
+				
+				<section id="tab1" class="tab is-current tabbed-content--container">
+					<div class="wrapper">
+						<div class="tabbed-content--body gd-row inner-content">
+							<div class="gd-third gd-columns">
+								<img class="float-center new-line" src="images/tab-mogosocial.png">
+							</div>
+							<div class="gd-two-thirds gd-columns">
 
-		<section class="mogo-nation border-divider">
-			<div class="wrapper align-center pvs">
-				<h3 class="great-text">Join MogoNation</h3>
-				<p class="paragon-text secondary-color pbl">Hundreds Are Already Following Us.  Are You?</p>
-			</div>
+								<h3 class="tabbed-nav--heading mogosocial great-text">Mogo<span>Social</span></h3>
+
+								<p class="primer-text thin">Have you ever looked online for a product only to spend a lot of time and not find what you need?</p>
+
+								<p class="primer-text thin">Now there is a better way and it is so simple.</p>  
+								
+								<ol class="primer-text thin">
+									<li>Submit your idea or request online.</li>
+									<li>Mogo Nation will vote and comment on it.</li>
+									<li>When the product has a large enough following you will get it on Mogobox.</li>
+								</ol>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<!-- Tab 2 -->
+				<section id="tab2" class="tab tabbed-content--container">
+					<div class="wrapper">
+						<div class="tabbed-content--body gd-row inner-content">
+							<div class="gd-third gd-columns">
+								<img class="float-center new-line" src="images/tab-mogoblog.png">
+							</div>
+							<div class="gd-two-thirds gd-columns">
+
+								<h3 class="tabbed-nav--heading mogoblog great-text">Mogo<span>Blog</span></h3>
+
+								<p class="primer-text thin">Looking for the inside scoop of the hottest upcoming inventions and products in the world?</p> 
+
+								<p class="primer-text thin"><strong>Follow, Read</strong> and <strong>Learn</strong> all about the newest, most unique products on the market. Hear about what a secret society of inventors are creating every month and get the inside scoop before your friends and family do.</p>
+
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- End Tab 2 -->
+
+				<!-- Tab 3 -->
+				<section id="tab3" class="tab tabbed-content--container">
+					<div class="wrapper">
+						<div class="tabbed-content--body gd-row inner-content">
+							<div class="gd-third gd-columns">
+								<img class="float-center new-line" src="images/tab-mogonation.png">
+							</div>
+							<div class="gd-two-thirds gd-columns">
+
+								<h3 class="tabbed-nav--heading mogonation great-text">Mogo<span>Nation</span></h3>
+
+								<p class="primer-text thin">Get live feeds and updates through your social media stay on top of the newest and greatest products <strong>IN</strong> and <strong>ENTERING</strong> the marketplace.</p> 
+
+								<p class="primer-text thin">MogoNation is all about keeping our followers up-to-date with trends, ideas, and the greatest products already in, and entering the market. Be part of our nation and share your products and ideas.</p>
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- End Tab 3 -->
+
+				<!-- Tab 4 -->
+				<section id="tab4" class="tab tabbed-content--container">
+					<div class="wrapper">
+						<div class="tabbed-content--body gd-row inner-content">
+							<div class="gd-third gd-columns">
+								<img class="float-center new-line" src="images/tab-mogobox.png">
+							</div>
+							<div class="gd-two-thirds gd-columns">
+
+								<h3 class="tabbed-nav--heading mogobox great-text">Mogo<span>Box</span></h3>
+
+								<p class="primer-text thin">Find and buy the newest gadgets, gizmos and products that you need and want. </p>
+ 
+								<p class="primer-text thin"><strong>Search, Find</strong> then <strong>Buy</strong>. MogoBox is an online store offering consumers products, technology, gadgets and gizmos based on trends across the USA. </p>
+
+							</div>
+						</div>
+					</div>
+				</section>
+				<!-- End Tab 4 -->	
+		</section>
+
+		<section class="mogo-nation">
+			<div class="wrapper align-center pvxl">
+				<h3 class="great-text mtn">Join MOGO<span class="bold">NATION</span></h3>
+				<p class="canon-text secondary-color mtxs">Hundreds Are Already Following Us.  Are You?</p>
+				<div class="gd-row">
+					<div class="gd-third gd-columns">
+						<a href="https://twitter.com/Mogobox" target="_blank" class="social-share--container twitter">
+							<i class="social-share__icon twitter"></i>
+							<span class="new-line align-left petite-text">@mogobox</span>
+							<span class="new-line align-left minion-text thin">Follow us on Twitter</span>
+						</a>
+					</div>
+					<div class="gd-third gd-columns">
+						<a href="https://www.facebook.com/Mogobox" target="_blank" class="social-share--container facebook">
+							<i class="social-share__icon facebook"></i>
+							<span class="new-line align-left petite-text">mogobox</span>
+							<span class="new-line align-left minion-text thin">Like us on Facebook</span>
+						</a>
+					</div>
+					<div class="gd-third gd-columns">
+						<a href="http://www.pinterest.com/mogobox/" target="_blank" class="social-share--container pinterest">
+							<i class="social-share__icon pinterest"></i>
+							<span class="new-line align-left petite-text">mogobox</span>
+							<span class="new-line align-left minion-text thin">Connect with us</span>
+						</a>
+					</div>
+				</div>
+			</div>		
 		</section>
 
 		<section class="mogo-submit">
-			<div class="wrapper">
+			<div class="wrapper inner-content">
 				<div class="gd-row">
-					<div class="gd-quarter gd-columns ptl">
+					<div class="gd-quarter gd-columns">
 						<img src="images/graphic-submit-box.png" />
 					</div>
 					<div class="gd-three-quarters gd-columns">
-						<h3 class="uppercase great-text">Submit Your Product</h3>
-						<p class="primer-text semibold">Have A GREAT Product You Are Trying To Promote Or Sell Online? Need Some Help Getting The Word Out?</p>
+						<h3 class="tertiary-heading uppercase great-text">Submit Your Product</h3>
+						<p class="paragon-text semibold mtxs">Have A GREAT Product You Are Trying To Promote Or Sell Online? <span class="new-line">Need Some Help Getting The Word Out?</span></p>
 						<p class="primer-text secondary-color">Fill out this form and we will contact you about offering your product on MogoBox.</p>
 					</div>
 				</div>
-				<div class="gd-row pvm">
-					<form action="" method="">
+				<form class="mogo-submit--form" method="POST" action="">
+					<div class="gd-row pvm">
+				
 						<div class="gd-half gd-columns">
-							<input type="text" name="name" placeholder="NAME">
-							<input type="email" name="email" placeholder="EMAIL">
-							<input type="url" name="website" placeholder="WEBSITE">
+							<input class="form-input" type="text" name="name" placeholder="NAME">
+							<input class="form-input" type="email" name="email" placeholder="EMAIL">
+							<input class="form-input" type="url" name="website" placeholder="WEBSITE">
 						</div>
 						<div class="gd-half gd-columns">
-							<textarea name="message" row="4" placeholder="TELL US ABOUT YOUR PRODUCT">
-							</textarea>
+							<textarea class="form-input message" name="message" placeholder="TELL US ABOUT YOUR PRODUCT"></textarea>
 						</div>
-						<div class="gd-full gd-columns pvs">
-							<button type="submit">Help Me Sell My Product!</button>
-						</div>
-					</form>
-				</div>
+						
+					</div>
+					<input class="button primary large gd-full mtn" type="submit" value="Help Me Sell My Product!">
+				</form>
+				
 			</div>
 		</section>
 
 		<section class="mogo-learn">
-			<div class="stretch-bg--mogo-crew align-center pts">
-				<h3 class="great-text white uppercase">Learn More About MogoNation</h3>
-				<h4 class="canon-text thin">Find Out Why We &amp; Our Partners Are So Successful</h4>
+			<div class="wrapper">
+				<div class="mogo-learn--background">
+					<h3 class="great-text align-center white uppercase">Learn More About MogoNation</h3>
+					<h4 class="canon-text align-center thin mtn">Find Out Why We &amp; Our Partners Are So Successful</h4>
+				</div>
+				<div class="gd-row">
+					<div class="gd-half gd-columns">
+						<div class="relative pvl phm align-center">
+							<div class="mogo-learn__icon answer">&nbsp;</div>
+							<h3 class="canon-text">Need an Item Fast?</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor ultricies pulvinar. Nam ornare iaculis sem, vel luctus eros cursus blandit. Vestibulum quis rhoncus sem, sit amet cursus mauris.</p>
+							<a class="button secondary large gd-full" href="tel:+18006646269">1-800-MOGOBOX</a>
+						</div>
+					</div>
+					<div class="gd-half gd-columns">
+						<div class="relative pvl phm align-center">						
+							<div class="mogo-learn__icon peek">&nbsp;</div>
+							<h3 class="canon-text">Request A Sneak Peek!</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor ultricies pulvinar. Nam ornare iaculis sem, vel luctus eros cursus blandit. Vestibulum quis rhoncus sem, sit amet cursus mauris.</p>
+							<a class="button secondary large gd-full" href="mailto:info@mogobox.com">Tell Me More Now!</a>
+						</div>
+					</div>
+				</div>
 			</div>
 		</section>
 
@@ -88,7 +238,7 @@
 	<footer>
 
 		<!-- Footer -->
-		<?php // include('viws/partials/footer.php'); ?>
+		<?php include('views/partials/footer.php'); ?>
 		<!-- End Footer -->
 
 	</footer>
--- templates/leaf-base/resources/js/actions.js
+++ templates/leaf-base/resources/js/actions.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+    $("body").addClass('js');
     
     windowScroll = {
         config: {
@@ -41,7 +43,90 @@ $(document).ready(function() {
         }
     };
 
+    homeSlider = {
+        config: {
+            targetElems: $(".js-slider"),
+            pluginOptions: {
+                "controlNav": true,
+                "directionNav": false,
+                "slideshowSpeed": 12000
+            }
+        },
+
+        init: function() {
+            var config = homeSlider.config;
+
+            if ( config.targetElems.length ) {
+                $.get("/templates/leaf-base/resources/js/jquery.flexslider.js")
+                    .done(function() {
+                        homeSlider.callSlider();
+                    }).fail(function() {
+                        config.targetElems.hide();
+                    });
+            }
+
+        },
+
+        callSlider: function() {
+            var context = homeSlider.config;
+
+            context.targetElems.flexslider( context.pluginOptions );
+        }
+    };
+
+    tabbedWidget = {
+        config: {
+            targetElems: $(".tabs"),
+            currentClass: "is-current"
+        },
+
+        init: function() {
+            var context = this.config,
+                revealNavItem = context.targetElems.find("[data-reveal]");
+
+            if ( context.targetElems.length ) {
+
+                revealNavItem.on("click", function( e ) {
+                    tabbedWidget.revealTab.call($(this), revealNavItem);
+                    e.preventDefault();
+                });
+
+            }
+        },
+
+        revealTab: function( item ) {
+            var thisButton = $(this),
+                thisData = thisButton.data("reveal"),
+                context = tabbedWidget.config,
+                tab = context.targetElems.find(".tab");
+
+            // Handles the classes on the navigation
+
+            if ( !thisButton.hasClass( context.currentClass ) ) {
+                // Remove is-current class from nav item
+                item.removeClass( context.currentClass );
+
+                // Add is-current class to item clicked
+                thisButton.addClass( context.currentClass );
+            }
+
+            // Handles Revealing Tabs
+            if ( tab.hasClass( context.currentClass ) ) {
+                tab.removeClass( context.currentClass );
+            }
+
+            context.targetElems.find("#" + thisData).addClass( context.currentClass );
+
+        }
+    };
+   
+
     // Call the script
+    homeSlider.init();
     windowScroll.init();
+    tabbedWidget.init();
+
+
+    
 
 });
